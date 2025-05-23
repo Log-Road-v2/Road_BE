88400d03b6624bdc1b929b8ac0a74b3346000f7d
hseun <hseun0732@dms.hs.kr>
2025-05-23 22:40:18 +0900

    chore :: userId type 변경

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,10 +12,14 @@ export interface PayloadData {
   iat: number;
 }
 
-export interface AuthenticatedRequest<Params = Record<string, never>, Query = ParsedQs, Body = Record<string, never>>
-  extends Request<Params, any, Body, Query> {
+export interface AuthenticatedRequest<
+  Params = Record<string, never>,
+  ResBody = unknown,
+  Body = Record<string, never>,
+  Query = ParsedQs
+> extends Request<Params, ResBody, Body, Query> {
   payload?: PayloadData;
-  userId?: number;
+  userId?: bigint;
 }
 
 export const REDIS_KEY = {
