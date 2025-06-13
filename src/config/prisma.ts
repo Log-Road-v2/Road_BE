16b5a932831c57c9788b6615e6981615bbff48a7
hseun <hseun0732@dms.hs.kr>
2025-06-13 14:15:48 +0900

    add :: enum import

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,6 @@
-import { PrismaClient, Role } from '@prisma/client';
+import { PrismaClient, Role, StudentState, ContestState, Author, ProjectState } from '@prisma/client';
 
 const prisma = new PrismaClient();
 console.log('Prisma has initted');
 
-export { prisma, Role };
+export { prisma, Role, StudentState, ContestState, Author, ProjectState };
