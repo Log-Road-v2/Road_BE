2fea3f4ce6f0250ddf86d1616c4c44376f7e9abc
limda00 <dylim7715@gmail.com>
2025-06-18 13:38:18 +0900

    add :: project controller 경로 추가

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import auth from './auth';
 import vote from "./vote"
+import project from './project';
 
 const app = express();
 
 app.use('/auth', auth);
 app.use('/vote', vote);
+app.use('/project', project);
 
 export default app;
