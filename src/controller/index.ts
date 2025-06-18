2a45f092f650864e6276a73b434015b24bc04ead
limda00 <dylim7715@gmail.com>
2025-06-18 16:45:56 +0900

    add :: user 경로 지정

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import auth from './auth';
 import vote from "./vote"
 import project from './project';
+import user from './user';
 
 const app = express();
 
 app.use('/auth', auth);
 app.use('/vote', vote);
 app.use('/project', project);
+app.use('/user', user);
 
 export default app;
