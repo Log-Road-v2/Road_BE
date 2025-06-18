8abdb17114209c7c4e159c402030daf68ff16353
은지 <wej0513@dsm.hs.kr>
2025-06-18 09:55:18 +0900

    add :: 투표 후보 조회 API

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -4,6 +4,7 @@ import vote from "./vote"
 import project from './project';
 import user from './user';
 import contest from './contest';
+import vote from "./vote"
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use('/vote', vote);
 app.use('/project', project);
 app.use('/user', user);
 app.use('/contest', contest);
+app.use('/vote', vote);
 
 export default app;
