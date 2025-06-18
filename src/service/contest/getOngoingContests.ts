1a6ad1a3fc8b9068796846810e70a27282678c01
limda00 <dylim7715@gmail.com>
2025-06-18 20:03:56 +0900

    add :: 진행중인 대회 조회 API

---
+++ src/service/contest/getOngoingContests.ts
@@ -0,0 +1,44 @@
+import { ContestState, prisma } from "../../config/prisma";
+import { Response } from "express";
+import { AuthenticatedRequest, BasicResponse } from "../../types";
+import { ContestData, getContestResponse } from "../../types/contest";
+import { formatDate } from "../../utils/regex";
+
+// 현재 진행중인 대회 목록 조회
+
+export const getOngoingContests = async (
+  req: AuthenticatedRequest,
+  res: Response<BasicResponse | getContestResponse>
+) => {
+  try {
+    const contests = await prisma.contest.findMany({
+      where: { state: ContestState.NOW },
+      select: {
+        id: true,
+        name: true,
+        startDate: true,
+        endDate: true
+      },
+      orderBy: { id: 'asc' }
+    })
+
+    const formattedContests: ContestData[] = contests.map((contest) => ({
+      id: contest.id.toString(),
+      name: contest.name,
+      startDate: formatDate(contest.startDate),
+      endDate: formatDate(contest.endDate)
+    }));
+
+    const response: getContestResponse = {
+      contests: formattedContests
+    }
+
+    return res.status(200).json(response);
+
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "서버 오류 발생",
+    });
+  }
+}
