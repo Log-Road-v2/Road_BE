99e361d2d71360602f7deb7f5179bae47865265e
limda00 <dylim7715@gmail.com>
2025-06-18 15:13:03 +0900

    add :: formatDate formatMember 함수

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,3 +1,5 @@
+import { StudentResponse } from "../types/project";
+
 export const checkEmailRegex = (email: string): boolean => {
   const emailPattern = /^[\w\-\.]{1,22}\@dsm.hs.kr$/;
   return emailPattern.test(email);
@@ -6,4 +8,15 @@ export const checkEmailRegex = (email: string): boolean => {
 export const checkPasswordRegex = (password: string): boolean => {
   const passwordPattern = /^[\w!@#$%^&*]{8,20}$/;
   return passwordPattern.test(password);
+};
+
+export const formatDate = (date: Date) => date.toISOString().split("T")[0];
+
+export const formatMembers = (rawMembers: any[]): StudentResponse[] => {
+  return rawMembers
+    .filter((m) => m.studentId !== null)
+    .map((m) => ({
+      studentId: m.studentId!,
+      name: m.student?.name,
+    }));
 };
