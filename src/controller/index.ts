b65dfbccadc1c137bda5c0c3ff43fdddd38006fb
limda00 <dylim7715@gmail.com>
2025-06-18 20:03:20 +0900

    chore :: controller 브랜치 충돌 해결

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,18 +1,14 @@
 import express from 'express';
 import auth from './auth';
-<<<<<<< feature/#17-Project
 import project from './project';
-=======
 import user from './user';
->>>>>>> develope
+import contest from './contest';
 
 const app = express();
 
 app.use('/auth', auth);
-<<<<<<< feature/#17-Project
 app.use('/project', project);
-=======
 app.use('/user', user);
->>>>>>> develope
+app.use('/contest', contest);
 
 export default app;
