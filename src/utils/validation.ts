83569fba298a38c089298ecd7006f8136e314552
hseun <hseun0732@dms.hs.kr>
2025-07-02 11:58:00 +0900

    chore :: validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,21 +1,10 @@
-import { RegisterProjectBody, ValidationResult } from "../types/project";
+import { RegisterProjectBody, StudentResponse, ValidationResult } from '../types/project';
 
 const isValidDate = (date: any) => !isNaN(new Date(date).getTime());
 
 export function validateProjectInput(body: RegisterProjectBody): ValidationResult {
-  const {
-    contestId,
-    projectName,
-    authorCategory,
-    skills,
-    members,
-    introduction,
-    description,
-    startDate,
-    endDate,
-    image,
-    video,
-  } = body;
+  const { contestId, projectName, authorCategory, skills, members, introduction, description, startDate, endDate } =
+    body;
 
   if (
     !contestId ||
@@ -24,34 +13,35 @@ export function validateProjectInput(body: RegisterProjectBody): ValidationResul
     !introduction?.trim() ||
     !description?.trim() ||
     !startDate ||
-    !endDate ||
-    !image ||
-    !video
+    !endDate
   ) {
-    return { valid: false, message: "필수 입력값이 누락되었습니다." };
+    return { valid: false, message: '필수 입력값이 누락되었습니다.' };
   }
 
   if (!isValidDate(startDate) || !isValidDate(endDate)) {
-    return { valid: false, message: "유효하지 않은 날짜 형식입니다." };
+    return { valid: false, message: '유효하지 않은 날짜 형식입니다.' };
   }
 
   if (new Date(startDate) > new Date(endDate)) {
-    return { valid: false, message: "시작일은 종료일보다 빠를 수 없습니다." };
+    return { valid: false, message: '시작일은 종료일보다 빠를 수 없습니다.' };
   }
 
-  const filteredSkills = skills?.filter((s) => s.trim().length > 0) || [];
+  const skill = JSON.parse(skills) as string[];
+  const filteredSkills = skill.filter((s) => s.trim().length > 0) || [];
   if (filteredSkills.length === 0) {
-    return { valid: false, message: "기술 스택을 입력해주세요." };
+    return { valid: false, message: '기술 스택을 입력해주세요.' };
   }
 
-  const filteredMembers = members?.filter((m) =>
-    m.studentId !== undefined
-  ).map((m) => ({
-    studentId: BigInt(m.studentId),
-  })) || [];
-  
+  const member = JSON.parse(members) as StudentResponse[];
+  const filteredMembers =
+    member
+      .filter((m) => m.studentId !== undefined)
+      .map((m) => ({
+        studentId: BigInt(m.studentId)
+      })) || [];
+
   if (filteredMembers.length === 0) {
-    return { valid: false, message: "프로젝트 멤버가 유효하지 않습니다." };
+    return { valid: false, message: '프로젝트 멤버가 유효하지 않습니다.' };
   }
 
   return { valid: true, filteredSkills, filteredMembers };
