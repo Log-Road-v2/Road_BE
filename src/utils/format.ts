0be343521d87bb66ad577b03f0475bd1368d4c7c
hseun <hseun0732@dms.hs.kr>
2025-07-02 13:43:40 +0900

    delete :: slash

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,4 +1,4 @@
 export const getRelativePath = (fullPath: string) => {
   const index = fullPath.indexOf('uploads');
-  return index !== -1 ? '/' + fullPath.slice(index) : fullPath;
+  return index !== -1 ? fullPath.slice(index) : fullPath;
 };
