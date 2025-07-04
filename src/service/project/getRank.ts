a793d977d60b15ecb4499eb4814b00aea8c6f31c
hseun <hseun0732@dms.hs.kr>
2025-07-04 13:47:35 +0900

    update :: getRank

--- src/service/project/getRank.ts
+++ src/service/project/getRank.ts
@@ -1,29 +1,28 @@
 import { prisma } from '../../config/prisma';
 import { Request, RequestHandler, Response } from 'express';
 import { BasicResponse } from '../../types';
-import { AwardResponse, RankProjectResponse, getRankResponse } from '../../types/project';
+import { AwardResponse, RankProjectResponse, GetRankResponse } from '../../types/project';
 import { ContestParams } from '../../types/contest';
+import { buildFileUrl } from '../../utils/buildFileUrl';
 
 // 수상작 조회
 
-export const rankingHandler: RequestHandler<ContestParams, BasicResponse | getRankResponse> = async (req, res) => {
+export const rankingHandler: RequestHandler<ContestParams, BasicResponse | GetRankResponse> = async (req, res) => {
   await ranking(req, res);
 };
 
 const ranking = async (
-  req: Request<ContestParams, BasicResponse | getRankResponse>,
-  res: Response<BasicResponse | getRankResponse>
+  req: Request<ContestParams, BasicResponse | GetRankResponse>,
+  res: Response<BasicResponse | GetRankResponse>
 ) => {
   try {
-    const contestIdParam = req.params.contestId;
-
-    if (!contestIdParam) {
+    const contestId = BigInt(req.params.contestId);
+    if (!contestId) {
       return res.status(400).json({ message: 'contestId가 필요합니다' });
     }
 
-    const contestId = BigInt(contestIdParam);
     const contest = await prisma.contest.findUnique({
-      where: { id: BigInt(contestId) },
+      where: { id: contestId, state: 'FINISHED' },
       select: {
         id: true,
         name: true,
@@ -38,31 +37,19 @@ const ranking = async (
       });
     }
 
-    const awards = await prisma.award.findMany({
-      where: { contestId: BigInt(contestId) },
+    const awardProjects = await prisma.award.findMany({
+      where: { contestId: contestId },
       select: {
-        name: true
-      }
-    });
-
-    const projects = await prisma.project.findMany({
-      where: {
-        contestId: BigInt(contestId),
-        awardProject: {
-          some: {}
-        }
-      },
-      select: {
-        id: true,
-        projectName: true,
-        authorCategory: true,
-        introduction: true,
-        image: true,
+        name: true,
         awardProject: {
           select: {
-            award: {
+            project: {
               select: {
-                name: true
+                id: true,
+                projectName: true,
+                authorCategory: true,
+                introduction: true,
+                image: true
               }
             }
           }
@@ -70,29 +57,29 @@ const ranking = async (
       }
     });
 
-    const formattedProjects: RankProjectResponse[] = projects.map((project) => ({
-      id: project.id.toString(),
-      projectName: project.projectName,
-      authorCategory: project.authorCategory,
-      introduction: project.introduction,
-      image: project.image,
-      award: project.awardProject.map((ap) => ap.award.name).join(', ')
-    }));
+    const projects: RankProjectResponse[] = awardProjects.flatMap((award) =>
+      award.awardProject.map((p) => ({
+        id: p.project.id.toString(),
+        projectName: p.project.projectName,
+        authorCategory: p.project.authorCategory,
+        introduction: p.project.introduction,
+        image: buildFileUrl(p.project.image),
+        award: award.name
+      }))
+    );
 
-    const formattedAwards: AwardResponse[] = awards.map((award) => ({
+    const awards: AwardResponse[] = awardProjects.map((award) => ({
       name: award.name
     }));
 
-    const response: getRankResponse = {
-      contestId: contest.id.toString(),
+    return res.status(200).json({
+      contestId: contestId.toString(),
       name: contest.name,
       startDate: contest.startDate,
       endDate: contest.endDate,
-      awards: formattedAwards,
-      projects: formattedProjects
-    };
-
-    return res.status(200).json(response);
+      awards: awards,
+      projects: projects
+    });
   } catch (err) {
     console.error(err);
     return res.status(500).json({
